4d0a591bd90e77b5b7cb2aa0b8520b6c6068c14a
katohassan <hassankato272@gmail.com>
2026-05-10 11:37:27 +0300

    Fixed HomeController types and implemented smart Admin redirection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,20 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using MatatuMVC.Models;
 
 namespace MatatuMVC.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly UserManager<User> _userManager;
+
+    public HomeController(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
     public IActionResult Index()
     {
         if (User.Identity?.IsAuthenticated == true)
@@ -20,9 +29,18 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Dashboard()
+    [Authorize]
+    public async Task<IActionResult> Dashboard()
     {
-        return View();
+        var user = await _userManager.GetUserAsync(User);
+        
+        // REDIRECT ADMINS TO COMMAND CENTER
+        if (user != null && (user.Role == Role.Admin || user.Role == Role.Owner))
+        {
+            return RedirectToAction("Index", "Admin");
+        }
+
+        return View(user);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
